8465c082473f84bde6aa5641427a3b846e68977a
Jorge Contreras <86329049+jorgeostos@users.noreply.github.com>
2023-07-13 09:45:41 -0600

    setup eslint

--- google-translate-clone/.eslintrc.cjs
+++ google-translate-clone/.eslintrc.cjs
@@ -1,27 +1,34 @@
-/* eslint-env node */
-
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:react-hooks/recommended',
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['standard-with-typescript', 'plugin:react/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}', '*.ts', '*.tsx'],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
   ],
-  parser: '@typescript-eslint/parser',
+  ignorePatterns: ['.eslintrc.cjs', 'vite.config.ts'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: true,
-    tsconfigRootDir: __dirname,
+    project: './google-translate-clone/tsconfig.json',
   },
-  plugins: ['react-refresh'],
+  plugins: ['react', '@typescript-eslint'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'space-before-function-paren': 'off',
+    '@typescript-eslint/space-before-function-paren': 'error',
   },
 }
